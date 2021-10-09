498f6468ff83813ef3ad1b81c9cd9bc81b4231db
Ömer Faruk Tutkun <66732899+OmerFarukTutkun@users.noreply.github.com>
2021-10-09 15:28:24 +0300

    add version number to id name

--- uci.h
+++ uci.h
@@ -116,7 +116,7 @@ void Uci_Loop() {
 	set_weights();
 	hash_table= (TTentry * )malloc(sizeof(TTentry)*HASH_SIZE);
 	memset(hash_table,0, 16*HASH_SIZE);
-	printf("id name Devre\n");
+	printf("id name Devre 1.0\n");
     printf("id author Omer Faruk Tutkun\n");
 	fflush(stdout);
 	int l=0;
@@ -155,7 +155,7 @@ void Uci_Loop() {
         } else if (!strncmp(line, "quit", 4) || info->quit) {
             break;
         } else if (!strncmp(line, "uci", 3)) {
-			printf("id name Devre\n");
+			printf("id name Devre 1.0\n");
     		printf("id author Omer Faruk Tutkun\n");
 			printf("option name Hash type spin default 16 min 2 max 512\n");
             printf("uciok\n");
