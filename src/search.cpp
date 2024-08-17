8c8a0368c94ba955a70643c104b91532db088fc3
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-08-17 17:03:15 +0300

    LMR (#37)
    Bench 2748536

--- src/search.cpp
+++ src/search.cpp
@@ -309,21 +309,23 @@ int Search::alphaBeta(int alpha, int beta, int depth, const bool cutNode, Thread
         if(ss->played > 3 && !PVNode && depth <= 5 && !SEE(*board, move, seeThreshold(isQuiet(move), depth))) {
             continue;
         }
+        int history = 0;
         lmr = 0;
-        if (ss->played > 2 && depth > 2 && isQuiet(move)) {
+        if (ss->played > 2 && depth > 2) {
             lmr = LMR_TABLE[depth][ss->played];
             lmr -= PVNode; //reduce less for PV nodes
             lmr += !improving;
-            lmr -= std::clamp(getQuietHistory(thread,ss, move)/8000, -2,2);
+            
+            if(isQuiet(move))
+                history = getQuietHistory(thread,ss, move);
+            else
+                history = getCaptureHistory(thread,ss, move);
+
+            lmr -= std::clamp(history/8000, -2,2);
             lmr += cutNode;
             lmr += ttMove && isTactical(ttMove);
         }
-        else if ( ss->played > 2 && depth > 2 && isTactical(move))
-        {
-            lmr = LMR_TABLE[depth][ss->played];
-            lmr -= std::clamp(getCaptureHistory(thread,ss, move)/8000, -2,2);
-            lmr += ttMove && isTactical(ttMove);
-        }
+        
         lmr = std::max(0, std::min(depth - 1, lmr));
         ss->continuationHistory = &thread.contHist[board->pieceBoard[moveFrom(move)]][moveTo(move)];
 
