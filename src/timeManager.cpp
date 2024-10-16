cf7de37efebd5af0ac158112a74b02b7a6c0a650
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-10-16 08:02:51 +0300

    Update timeManager.cpp

--- src/timeManager.cpp
+++ src/timeManager.cpp
@@ -49,7 +49,7 @@ void TimeManager::start() {
     auto moveOverhead = Options.at("MoveOverhead");
     hardTime =
       remainingTime * hardTimePercentage / 100 + inc - std::stoi(moveOverhead.currentValue);
-harTime = std::min(hardTime, 80*remainingTime/100);
+hardTime = std::min(hardTime, 80*remainingTime/100);
     softTime =
       remainingTime * softTimePercentage / 100 + inc - std::stoi(moveOverhead.currentValue);
 }
