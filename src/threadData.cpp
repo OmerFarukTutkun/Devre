78d71707a169c2b46d7c6606d6ca369d0456eb01
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-10-24 00:05:47 +0300

    Bench 2269896

--- src/threadData.cpp
+++ src/threadData.cpp
@@ -1,11 +1,13 @@
 #include "threadData.h"
 
 ThreadData::ThreadData(const std::string& fen, int ID) {
+    std::fill_n(&history[0][0][0][0][0], 2*2*2*64*64, -500);
     this->board = Board(fen);
     ThreadID    = ID;
 }
 
 ThreadData::ThreadData(Board& b, int ID) {
+    std::fill_n(&history[0][0][0][0][0], 2*2*2*64*64, -500);
     board    = Board(b);
     ThreadID = ID;
 }
