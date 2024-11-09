31457241e4757a76501c6be4c897ed5d90b77cd5
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2024-11-09 13:48:17 +0300

    Bench 2913387

--- src/search.cpp
+++ src/search.cpp
@@ -463,7 +463,7 @@ int Search::alphaBeta(int alpha, int beta, int depth, const bool cutNode, Thread
         int extension = 0;
         if (ss->ply < thread.searchDepth && !rootNode && depth >= 8 && move == ttMove && ss->excludedMove == NO_MOVE && (ttBound & TT_LOWERBOUND) && ttDepth >= depth - 3)
         {
-            const int singularBeta  = ttScore - 4 * depth;
+            const int singularBeta  = ttScore - depth;
             const int singularDepth = (depth - 1) / 2;
 
             ss->excludedMove  = move;
