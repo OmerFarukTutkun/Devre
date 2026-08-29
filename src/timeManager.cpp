11918094d7c2126a34b599e1051abd5e0fc2e7d3
Ömer Faruk Tutkun <lrctutfaruk@gmail.com>
2026-08-29 12:45:08 +0300

    spend more time
    bench 1203274

--- src/timeManager.cpp
+++ src/timeManager.cpp
@@ -3,8 +3,8 @@
 #include "util.h"
 #include "tuning.h"
 
-DEFINE_PARAM_B(hardTimePercentage, 40, 0, 100);
-DEFINE_PARAM_B(softTimePercentage, 5, 0, 100);
+DEFINE_PARAM_B(hardTimePercentage, 45, 0, 100);
+DEFINE_PARAM_B(softTimePercentage, 6, 0, 100);
 
 bool TimeManager::checkLimits(uint64_t totalNodes) {
 
